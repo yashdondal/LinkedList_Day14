1d226b328f119bd62d6c47e3cfa7e8e3e8d2765a
yashdondal <yashdondal21@gmail.com>
2022-10-20 17:09:15 +0530

    Inserting Add

--- LinkedList.cs
+++ LinkedList.cs
@@ -29,8 +29,73 @@ namespace LinkedList
             Console.WriteLine("{0} inserted into linked list", node.data);
 
         }
-        public void Display()
+
+
+        public void AddInReverseOrder(int data)            // To Display Add Elements In Reverse Order
         {
+            Node newNode = new Node(data);
+            if (this.head == null)
+            {
+                this.head = newNode;
+            }
+            else
+            {
+                Node temp = this.head;
+                head = newNode;
+                head.next = temp;
+            }
+            Console.WriteLine("{0} inserted into linked list  ", newNode.data);
+        }
+
+        public void Append(int data)
+        {
+            Node node = new Node(data);
+            if (head == null)
+            {
+                head = node;
+            }
+            else
+            {
+                Node node1 = head;
+                while (node1.next != null)
+                {
+                    node1 = node1.next;
+                }
+                node1.next = node;
+            }
+        }
+
+        internal Node InsertAtParticularPosition(int position, int data)        // Insert Element In Givien Position
+        {
+            Node newestNode = new Node(data);
+            if (this.head == null)
+            {
+                return newestNode;
+            }
+            if (position == 0)
+            {
+                newestNode.next = this.head;
+                this.head = newestNode;
+                return this.head;
+            }
+            Node? prev = null;
+            Node current = this.head;
+            int count = 0;
+            while (current != null && count < position)
+            {
+                prev = current;
+                current = current.next;
+                count++;
+            }
+            newestNode.next = prev.next;
+            prev.next = newestNode;
+            return this.head;
+        }
+
+
+
+        public void Display()
+            {
             Node temp = this.head;
             if (temp == null)
             {
--- Program.cs
+++ Program.cs
@@ -1,7 +1,8 @@
-﻿namespace LinkedList
+﻿namespace Day14_PracticalProblem
 {
     public class Program
     {
+
         static void Main(string[] args)
         {
             LinkedList linkedList = new LinkedList();
@@ -10,6 +11,30 @@
             linkedList.Add(70);
             linkedList.Display();
 
+
+            LinkedList linkedList1 = new LinkedList();
+
+            linkedList1.AddInReverseOrder(70);
+            linkedList1.AddInReverseOrder(30);
+            linkedList1.AddInReverseOrder(56);
+            linkedList1.Display();
+
+            LinkedList linkedList2 = new LinkedList();
+
+            linkedList2.Append(56);
+            linkedList2.Append(30);
+            linkedList2.Append(70);
+            linkedList2.Display();
+
+            LinkedList linkedList3 = new LinkedList();
+
+            linkedList.Add(56);
+            linkedList.Add(70);
+            linkedList.InsertAtParticularPosition(1, 30);
+            linkedList.Display();
+
         }
+
+
     }
 }
